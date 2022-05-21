dd0cf727bcedf22f543ba9ad5e83917d51775ffa
Chaoyi <zhuchaoyibuaa@gmail.com>
2022-05-21 17:27:24 +0200

    add cifar10 support

--- fltk/nets/cifar_10_cnn.py
+++ fltk/nets/cifar_10_cnn.py
@@ -1,9 +1,38 @@
 # pylint: disable=missing-class-docstring,invalid-name
 import torch
 import torch.nn.functional as F
-
+import torch.nn as nn
 class Cifar10CNN(torch.nn.Module):
+    def __init__(self):
+        super(Cifar10CNN, self).__init__()
+        self.conv1 = nn.Conv2d(3, 6, 5)
+        self.pool = nn.MaxPool2d(2, 2)
+        self.conv2 = nn.Conv2d(6, 16, 5)
+        self.fc1 = nn.Linear(400, 120)
+        self.fc2 = nn.Linear(120, 84)
+        self.fc3 = nn.Linear(84, 10)
+        self.feature_fc1 = None
+        self.feature_fc2 = None
+        self.feature_fc3 = None
+        #self.feature_fc1_graph = None
+        #self.feature_fc2_graph = None
+        #self.feature_fc3_graph = None
 
+    def forward(self, x):
+        x = self.pool(F.relu(self.conv1(x)))
+        x = self.pool(F.relu(self.conv2(x)))
+        x = x.view(x.size(0), -1)
+        #self.feature_fc1_graph = x
+        self.feature_fc1 = x.cpu().detach().numpy()
+        x = F.relu(self.fc1(x))
+        #self.feature_fc2_graph = x
+        self.feature_fc2 = x.cpu().detach().numpy()
+        x = F.relu(self.fc2(x))
+        #self.feature_fc3_graph = x
+        self.feature_fc3 = x.cpu().detach().numpy()
+        x = self.fc3(x)
+        return F.log_softmax(x, dim=1)
+'''
     def __init__(self):
         super(Cifar10CNN, self).__init__()
 
@@ -49,3 +78,4 @@ class Cifar10CNN(torch.nn.Module):
         x = self.softmax(self.fc2(x))
 
         return x
+'''
